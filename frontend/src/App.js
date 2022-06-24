aa92f32e17ca3ec46599c24fb3cf292e22208559
maya-58 <manimacrack@gmail.com>
2022-06-24 19:17:57 +0530

    Bootstarp working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = ()=> {
   return (
-    <h1>
-      welcome to the proshop
-    </h1>
+    <>
+    <Header />
+      <main>
+        <Container>
+        <h1>welcome to the proshop</h1> 
+        </Container>
+     
+      </main>
+    <Footer />
+    </>
   );
 }
 
