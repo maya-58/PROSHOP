b8ea1c5782ce39db1bfcbb1e7553a213fcc28300
maya-58 <manimacrack@gmail.com>
2022-06-25 18:59:52 +0530

    axios es6 import running 1

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,4 +1,4 @@
-const { Module } = require("module")
+//const { Module } = require("module")
 
 const products = [
   {
@@ -81,4 +81,4 @@ const products = [
   },
 ]
 
-module.exports= products
+export default products
--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import { Link, useParams } from "react-router-dom";
 import {
   Row,
@@ -10,12 +10,24 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import Rating from "../Rating";
-import products from "../../products";
+//import products from "../../products";
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
   const { id } = useParams();
   //console.log(id);
-  const product = products.find((p) => p._id === id);
+ // const product = products.find((p) => p._id === id);
+
+ const [product,setProduct] = useState({})
+
+ useEffect(()=>{
+  const fetchProduct = async () => {
+    const {data} = await axios.get(`/api/products/${id}`)
+
+    setProduct(data)
+  }
+  fetchProduct()
+},[])
 
   //console.log(product);
 
