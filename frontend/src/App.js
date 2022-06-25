b6bc38a088691ee0dbb087f0587bdce954930763
maya-58 <manimacrack@gmail.com>
2022-06-25 17:01:30 +0530

    Implemented react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from '../src/components/screens/HomeScreen';
+import ProductScreen from '../src/components/screens/ProductScreen';
 
 const App = ()=> {
   return (
-    <>
+    <Router>
     <Header />
       <main>
-        <Container>
-        <h1>welcome to the proshop</h1> 
-        </Container>
-     
+        <Container> 
+          <Routes>       
+          <Route path='/' element={<HomeScreen />} exact />
+          <Route path='/product/:id' element={<ProductScreen />} />         
+          </Routes>  
+        </Container>     
       </main>
     <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/screens/ProductScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {Row,Col,Image,ListGroup,Card,Button} from 'react-bootstrap'
+import Rating from '../Rating'
+import products from '../../products'
+
+const ProductScreen = ({match}) => {
+
+  //const product = product.find((p) => p._id === match.params._id)
+
+
+  return (
+
+
+
+    <div>
+     llllll
+    </div>
+  )
+}
+
+export default ProductScreen
